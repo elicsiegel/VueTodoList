661b6942b94f582fc60955fe7be50d9fa26097a2
Eli Siegel <eli.siegel@lyquix.com>
2018-10-10 13:51:12 -0400

    Adjust header list

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,17 @@
 <template>
   <div>
-    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
-    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <div class='ui centered card' id="card-header">
+      <div class='content'>
+        <div class='ui form'>
+          <div class='field'>
+            <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+          </div>
+          <div class='field'>
+            <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
     <todo v-on:delete-todo="deleteTodo" v-on:complete-todo="completeTodo" v-for="todo in todos" :todo="todo"></todo>
   </div>
 </template>
@@ -27,4 +37,8 @@
   };
 </script>
 <style>
+
+  #card-header {
+    margin-top: 10px;
+  }
 </style>
